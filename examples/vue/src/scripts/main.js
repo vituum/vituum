382cb91b043f4b5bd2f30d02aa84b8b443627585
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-24 09:48:36 +0200

    feat: added vue example

---
+++ examples/vue/src/scripts/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from '../templates/App.vue'
+
+createApp(App).mount('#app')
---
+++ examples/vue/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vituum'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [vue()]
+})
