3a8860c08d00c67c65f1a72a00702dbdf607f40a
Rafal Galka <rafal.galka@modernweb.pl>
2023-12-10 16:15:57 +0100

    fix: path resolution when `vite.root` is set
    Resolves #33

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
+import { join } from 'node:path'
 import pluginPages, { defaultConfig as defaultConfigPages } from './plugins/pages.js'
 import pluginImports, { defaultConfig as defaultConfigImports } from './plugins/imports.js'
 import { resolveInputPaths } from './utils/build.js'
-import { merge } from './utils/common.js'
+import { merge, normalizePath } from './utils/common.js'
+
+const TEMPLATE_FORMATS = ['latte', 'twig', 'liquid', 'njk', 'hbs', 'pug']
+const templateExtensions = (...additionalFormats) => ([...additionalFormats, ...TEMPLATE_FORMATS].join(','))
 
 const defaultConfig = {
-    input: [
-        './src/styles/*.{css,pcss,scss,sass,less,styl,stylus}',
-        './src/scripts/*.{js,ts,mjs}'
-    ],
-    formats: ['json', 'latte', 'twig', 'liquid', 'njk', 'hbs', 'pug'],
+    input: [],
+    formats: ['json', ...TEMPLATE_FORMATS],
     pages: defaultConfigPages,
     imports: defaultConfigImports
 }
-
-const defaultInput = [
-    'index.html',
-    './src/pages/**/*.{json,latte,twig,liquid,njk,hbs,pug,html}',
-    '!./src/pages/**/*.{latte,twig,liquid,njk,hbs,pug,html}.json'
-]
 
 /**
  * @param {import('vituum').UserConfig} pluginUserConfig
  * @returns {import('vite').Plugin}
  */
 const pluginCore = (pluginUserConfig) => {
+    const pagesRoot = normalizePath(pluginUserConfig.pages.root)
+    const pagesPath = normalizePath(pluginUserConfig.pages.dir)
+
+    const defaultInput = [
+        'index.html',
+        join(pagesPath, `**/*.{${templateExtensions('json', 'html')}}`),
+        '!' + join(pagesPath, `**/*.{${templateExtensions('html')}}.json`),
+        join(pagesRoot, 'styles/*.{css,pcss,scss,sass,less,styl,stylus}'),
+        join(pagesRoot, 'scripts/*.{js,ts,mjs}'),
+        ...pluginUserConfig.input
+    ]
     let userConfig
 
     return {
@@ -40,7 +46,6 @@ const pluginCore = (pluginUserConfig) => {
             if (userConfig?.build?.rollupOptions?.input) {
                 userConfig.build.rollupOptions.input = resolveInputPaths({ paths: userConfig.build.rollupOptions.input, root: userConfig.root }, pluginUserConfig.formats)
             } else {
-                defaultInput.push(...pluginUserConfig.input)
                 userConfig.build = userConfig.build || {}
                 userConfig.build.rollupOptions = userConfig.build.rollupOptions || {}
                 userConfig.build.rollupOptions.input = resolveInputPaths({ paths: defaultInput, root: userConfig.root }, pluginUserConfig.formats)
